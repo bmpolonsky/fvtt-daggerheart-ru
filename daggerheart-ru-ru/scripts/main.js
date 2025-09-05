d04e72cebd6a78431a40e2d5c308de59f1c202c3
Ujin <37151086+LooseSlives@users.noreply.github.com>
2025-09-05 17:07:48 +0300

    Add files via upload
    Добавил журналы:
     ОСНОВНЫЕ МЕХАНИКИ и ВЕДЕНИЕ ИГРЫ

--- daggerheart-ru-ru/scripts/main.js
+++ daggerheart-ru-ru/scripts/main.js
@@ -11,11 +11,14 @@ Hooks.once('babele.init', (babele) => {
 
         "toAdversariesItems": (original, translation) => {
             for (i in original) {
-                original[i].name = translation[original[i]._id]?.name;
-                original[i].system.description = translation[original[i]._id]?.description;
+								if (original[i]._id in translation){
+									original[i].name = translation[original[i]._id]?.name;
+									original[i].system.description = translation[original[i]._id]?.description;
+								}
             }
             return;
         }
+				
     });
 
 });
